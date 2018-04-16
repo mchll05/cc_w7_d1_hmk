97a2a1b3513ae8a7c75509d3d99e2aae3d69cdcb
Michelle Austin <mchllaustin@gmail.com>
2018-04-16 16:38:24 +0100

    Created Visitor

---
+++ src/main/java/Visitor.java
@@ -0,0 +1,12 @@
+public class Visitor {
+
+    int age;
+    double height;
+    double money;
+
+    public Visitor (int age, double height, double money){
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+}
